63d3e409f9fca478cd47a8fb9122618c0122fe1e
kern3020@gmail.com <kern3020@gmail.com>
2014-02-26 15:57:10 -0800

    wip -added new tests.

--- pattern_matching/exact.cpp
+++ pattern_matching/exact.cpp
@@ -9,17 +9,12 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <string>
 
 using namespace std;
 
-// start with 
-// http://en.wikipedia.org/wiki/Boyer%E2%80%93Moore%E2%80%93Horspool_algorithm
-
 list<int> exact_match(const string &pat, const string &text) {
   list<int> indices_found; // index (or indices) for each match.
-  int n = pat.length(); 
-  int last = n - 1; // position of last char in pattern
-  int m = text.length();
 
   // given a character and index in a pattern, how much should skip it? 
   map<char, list<int> > charToSkipDistance; // todo: should it restrict it to genomic alphabet?
@@ -41,7 +36,7 @@ list<int> exact_match(const string &pat, const string &text) {
   map<char, list<int> >::iterator pos; 
   for (pos = charToSkipDistance.begin(); pos != charToSkipDistance.end(); ++pos){
     cout << "key: " << pos->first << "\t" 
-	 << "value: " ;
+  	 << "value: " ;
     list<int>::iterator list_pos;
     for(list_pos = pos->second.begin(); list_pos != pos->second.end(); ++list_pos){
        cout << *list_pos << " "; 
@@ -50,9 +45,42 @@ list<int> exact_match(const string &pat, const string &text) {
   }
 
   // search 
-  
+  int n = pat.length() - 1 ; 
+  int m = text.length() - 1;
+  int k = n; // k is index of the last element in the text
+  while (k <= m) {
+    int i = n;
+    int h = k;
+    while (i > 0 && pat[i] == text[h]) {
+      i--;
+      h--;
+    }
+    if (i == 0) {
+      // found match 
+      indices_found.push_front(h);
+      if( k + n > m) {
+	k = m;
+      } else { 
+	k = k + n + 1 ;
+      }
+    } else {
+      // apply bad character rule 
+      char ch = text[h];
+      if (pat.find(ch) == string::npos){
+	k += n + 1; 
+      } else {
+	list<int> indices = charToSkipDistance[ch];
+	list<int>::iterator list_pos;
+	for(list_pos = indices.begin(); list_pos != indices.end(); ++list_pos){
+	  if (*list_pos < i) {
+	    k += *list_pos + 1;
+	    break; 
+	  }
+	}
+      }
+    }
+  }
 
   return indices_found; 
-
 }
 
--- pattern_matching/exact_test.cpp
+++ pattern_matching/exact_test.cpp
@@ -1,8 +1,60 @@
 #include "gtest/gtest.h"
 #include "exact.h"
 
+#include <algorithm>
+#include <cerrno>
+#include <cstdlib>
+#include <cstring> 
+#include <fstream>
+#include <iostream>
+#include <list>
+#include <map>
+#include <string>
+
+
 TEST(ExactTest, SimpleCommonCase) {
   list<int> exp;
   exp.push_front(9);
   EXPECT_EQ(exp, exact_match("BARB", "FOOBUBBABBARBIZBAZ"));
 }
+
+TEST(ExactTest, SimpleSeqCase) {
+  ifstream fasta_file("test_data/foo.fasta");
+  int seq_size = 512;
+  int line_size = 128;
+  char *buf = new char(line_size); 
+  string seq;
+  
+  if ( !fasta_file) {
+    FAIL() << "can't open input file \"" << fasta_file << "\"";
+  }
+
+  // ignore - header which is the first line of the fasta file.
+  char c;
+  do {
+    fasta_file.get(c);
+  } while (fasta_file.eof() || c != '\n');
+
+  // begining of range to read
+  int beg_pos = fasta_file.tellg(); 
+
+  // end of range
+  fasta_file.seekg(0, std::ios::end); 
+  int end_pos = fasta_file.tellg();
+
+  // resize
+  int s = end_pos - beg_pos;
+  seq.resize(s);
+
+  //reset to begining of range
+  fasta_file.seekg(beg_pos);
+  fasta_file.read(&seq[0], s);
+  
+  seq.erase( std::remove_if( seq.begin(), seq.end(), ::isspace ), seq.end() );
+
+  list<int> exp;
+  exp.push_front(9);
+  
+  EXPECT_EQ(exp, exact_match("GTA", seq)); // CAATA
+}
+
